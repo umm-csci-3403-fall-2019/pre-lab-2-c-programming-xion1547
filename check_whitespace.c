c1a1635c11de7e7c6f8df077eddd718bfb27e3fa
xion1547 <xion1547@morris.umn.edu>
2019-09-18 23:34:46 -0500

    Adds if statement to check if cleaned's result has any length. It's a string and if it has length, that means it has memory because of strip. Adds free(cleaned); to the if statement.

--- check_whitespace.c
+++ check_whitespace.c
@@ -66,7 +66,9 @@ int is_clean(char* str) {
   // 0 if they're equal, and a positive value if the first is
   // greater than the second.
   result = strcmp(str, cleaned);
-
+  if (strlen(cleaned) != 0) {
+	  free(cleaned);
+  }
   return result == 0;
 }
 
